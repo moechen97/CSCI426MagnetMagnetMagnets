9efc7ab2c8237e98f1c8985ec7e7d46fbd40cc71
moechen97 <69623061+moechen97@users.noreply.github.com>
2021-11-16 21:12:48 -0800

    add volume adjust and  adjust lock in level2

---
+++ CSCI426Prototype8/Assets/YimengNewWorkSpace/VolumeController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        slider.value = AudioListener.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeVolume(float x)
+    {
+        AudioListener.volume = x;
+    }
+}
