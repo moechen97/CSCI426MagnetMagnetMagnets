97d05bd21475d216f86ec55c3d3bc4d88ebeb3e9
Ryan Frazier <ryanfraz@usc.edu>
2021-11-23 20:06:14 -0800

    menu variable saver levels

--- CSCI426Prototype8/Assets/MENU/MENU_LevelButton.cs
+++ CSCI426Prototype8/Assets/MENU/MENU_LevelButton.cs
@@ -2,26 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MENU_LevelButton : MonoBehaviour
 {
+    private VariablesSaver vs;
     private string levelScene;
+    private int number;
     // Start is called before the first frame update
     void Start()
     {
+        vs = GameObject.FindGameObjectWithTag("VariablesSaver").GetComponent<VariablesSaver>();
         string[] nameInfo = gameObject.name.Split('_');
         if (nameInfo.Length == 2)
         {
             levelScene = nameInfo[0] + nameInfo[1];
+            number = int.Parse(nameInfo[1]) - 1;
         }
         else
         {
             levelScene = nameInfo[0];
+            number = -1;
+        }
+
+        bool seen = false;
+        if (number != -1)
+        {
+            for (int i = 0; i < vs.levelsCompleted.Length; i++)
+            {
+                if (!vs.levelsCompleted[i])
+                {
+                    break;
+                }
+                if (i == number)
+                {
+                    transform.GetChild(0).GetComponent<Image>().color = Color.green;
+                    seen = true;
+                }
+            }
+        }
+
+        if(!seen && number > 0 && number < vs.levelRange)
+        {
+            if(!vs.levelsCompleted[number])
+            {
+                transform.GetChild(0).GetComponent<Image>().color = Color.red;
+            }
         }
     }
 
     public void OnClick()
     {
-        SceneManager.LoadScene(levelScene);
+        if(number > 0 && number <= vs.levelRange)
+        {
+            SceneManager.LoadScene(levelScene);
+        }
+        else if(number == 0)
+        {
+            SceneManager.LoadScene(levelScene);
+        }
     }
 }
--- CSCI426Prototype8/Assets/VariablesSaver.cs
+++ CSCI426Prototype8/Assets/VariablesSaver.cs
@@ -7,13 +7,21 @@ public class VariablesSaver : MonoBehaviour
 {
     public int levelRange;
     [HideInInspector] public int level;
+    [HideInInspector] public bool[] levelsCompleted;
+    [HideInInspector] public int currentLevel;
     void Awake()
     {
         string sceneName = SceneManager.GetActiveScene().name;
-        if (!sceneName.Equals("Menu")) level = sceneName[sceneName.Length - 1] - '0';
-        else level = 0;
+        if (!sceneName.Equals("New Menu")) level = sceneName[sceneName.Length - 1] - '0';
+        else level = 0; 
         Debug.Log("VARIABLE SAVER: Begin level " + level);
         DontDestroyOnLoad(this.gameObject);
+        levelsCompleted = new bool[levelRange];
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("New Menu");
     }
 
     public void ReloadLevel()
@@ -23,6 +31,7 @@ public class VariablesSaver : MonoBehaviour
 
     public void NextLevel()
     {
+        levelsCompleted[level] = true;
         if(level < levelRange) level++;
         SceneManager.LoadScene("Level" + level);
     }
